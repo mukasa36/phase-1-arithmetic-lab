7a310999f7c9076048980fe2609f20aaf40d2423
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-06-14 19:19:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1=31;
 
-// Write your code here
+const num2=2;
+
+let multiply =num1 * num2;
+
+let random =Math.floor(Math.random()*10);
+
+let max =Math.max(1,2,4,6,7,20);
+
+let num3=24;
+let num4=5;
+let mod = num3 % num4;
+
+console.log(random);
+
+
+
